690b132f54939a7a711ed0378be0816c963c1aa0
tanada@sandagakuen.ed.jp <tanada@sandagakuen.ed.jp>
2024-09-02 15:20:15 +0900

    update

--- StudyGamePrograming08/Game.cpp
+++ StudyGamePrograming08/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-#include <gl/glew.h>
+#include <glew.h>
 #include "Texture.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -12,6 +12,8 @@
 #include "ClearPict.h"
 #include "Random.h"
 #include "InputSystem.h"
+#include <thread>
+#include <chrono>
 //#include <SDL.h>
 //#include <SDL_image.h>
 
@@ -157,17 +159,15 @@ void Game::ProcessInput()
 
 void Game::UpdateGame()
 {
-	// デルタタイムの計算
-	// 前のフレームから 16ms 経つまで待つ
-	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16));
-
-	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
-
-	// デルタタイムを最大値で制限する
-	if (deltaTime > 0.05f)
-	{
-		deltaTime = 0.05f;
+	// フレームレート調整（62.5fps)
+	if (SDL_GetTicks() - mTicksCount < 16) {
+		int sleep = 16 - (SDL_GetTicks() - mTicksCount);
+		std::this_thread::sleep_for(std::chrono::milliseconds(sleep));    // sleepミリ秒処理を止める
 	}
+
+	//while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16));	// 前のフレームから 16ms 経つまで待つ.※sleepしないのでCPU使用率が上がる。
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;		// デルタタイムの計算
+	if (deltaTime > 0.05f) { deltaTime = 0.05f; }			// デルタタイムを最大値で制限する
 	mTicksCount = SDL_GetTicks();
 
 	// すべてのアクターを更新
